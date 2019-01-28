96fa3f451bb64f93d811c845ef4039b88136efa4
Ammar Ali <funnyguy118@gmail.com>
2019-01-28 03:05:43 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,9 @@
 def reverse_each_word (sentence)
-  sentence_ary = sentence.split(/ /)
-  reversed_sentence_ary = sentence_ary.collect {|word| word = word.reverse}
-  reversed_sentence_ary.join(" ")
-  
-  #sentence_ary = sentence.split(/ /)
-  #updated_sentence = []
+  sentence.split.collect{|word| word.reverse}.join(" ")
+end
+
+  #sentence_ary = sentence.split
   #sentence_ary.each do |word|
   #  sentence_ary << word.reverse 
   #end 
   #sentence_ary.join(" ")
-end
