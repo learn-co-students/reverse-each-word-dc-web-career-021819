36aa2a620614ab7fbd5fee20b668a7d59b7416dc
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-06 17:22:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ sentence.each(&:reverse!)
 end 
 
 def reverse_each_word(sentence)
-sentence = ["Hello", "there,", "and", "how", "are", "you?"]
+sentence = ["Hi", "again,", "just", "making", "sure", "its", "reversed!"]
 sentence.collect(&:reverse!)
 end 
 
