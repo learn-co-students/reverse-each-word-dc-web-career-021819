92941e9fe558a46843d653d5499152fa8d6508f0
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-06 17:07:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
 def reverse_each_word(sentence)
-  arr, new_arr = ["Hello", "there", "and", "how", "are", "you?"], []
-for i in 0...arr.length do
-   new_arr << arr[arr.length - 1 - i]
-end
-new_arr
-
+sentence = []
+a = ["hi", "hey", "hello"]
+a.map(&:reverse!)
 end 
