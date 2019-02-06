af491313ba197f26d38af93e48874107190d48ee
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-06 17:06:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
 def reverse_each_word(sentence)
-  
+  arr, new_arr = ["Hello", "there", "and", "how", "are", "you?"], []
+for i in 0...arr.length do
+   new_arr << arr[arr.length - 1 - i]
+end
+new_arr
 
 end 
