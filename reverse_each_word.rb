25dce5d92108f47cd3c2661f151154cb1127134d
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-06 17:27:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,9 @@
-def reverse_each_word(s)
-sentence = []
-sentence.each(&:reverse!)
-end 
-
 def reverse_each_word(sentence)
-sentence = []
+sentence = sentence.split(" ")
 sentence.collect(&:reverse!)
 end 
+
+
 
 
 
